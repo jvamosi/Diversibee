edb3694c071017bd3c6de72ff5123053de5ab26d
ginxwar <kevin.wu@gmail.com>
2014-09-02 16:58:03 -0700

    added crop production

--- index.js
+++ index.js
@@ -3,15 +3,24 @@ var clone = require('clone');
 var neighbor = require('./neighbor').neighbor;
 
 var util = require('util');
-util.inspect(util, { showHidden: true, depth: null })
+util.inspect(util, {
+  showHidden: true,
+  depth: null
+})
 
 
 var c = function(myObject) {
-  console.log(util.inspect(myObject, {showHidden: false, depth: null}));
+  console.log(util.inspect(myObject, {
+    showHidden: false,
+    depth: null
+  }));
 }
 
 //TODO: each forest should have a max population size that bees can't exceed
 
+
+// initial yield for crop output
+var cropYield = 0;
 
 // forest block
 var F = {
@@ -25,7 +34,8 @@ var F = {
 
 // crop block
 var C = {
-  type: "crop"
+  type: "crop",
+  yield: 100
 };
 
 
@@ -34,27 +44,46 @@ var intrinsicRateOfIncrease = 0.1;
 
 
 var field = [
-  [F, F, C],
-  [C, C, F],
-  [C, F, C]
+  [F, F],
+  [C, C]
 ];
 
 
 
 console.log("initial field *****");
 c(field);
+console.log("*****");
 
 
 
-var advanceTimeStep = function() {
-  //determine crop production
-  //determine bee production
+
+var polinationPercentage = function(beeCount) {
+  // polination = base + scale(1-e(-N/1000))
+  // N = number of bees
+  return 0 + 100 * (1 - Math.pow(Math.E, beeCount * -1 / 1000));
 }
 
+// c(polinationPercentage(100));
 
 
+var advanceCropProduction = function(field) {
+  _(field).each(function(row, rowIndex) {
 
-var advanceCropProduction = function() {
+    _(row).each(function(cell, cellIndex) {
+
+      if (cell.type === "crop") {
+
+        // determine how many forest neighbors are there
+        var neighbors = neighbor(field, rowIndex, cellIndex);
+
+        var crop = clone(cell);
+
+        cropYield += polinationPercentage(neighbors.capacity);
+
+      }
+
+    }) //cell
+  }); //row
 
 }
 
@@ -64,17 +93,14 @@ var advanceBeePopulation = function(field) {
     _(row).each(function(cell, cellIndex) {
 
       if (cell.type === "forest") {
-        
+
         var forest = clone(cell);
 
-        // determine how many neighbors are there
+        // determine how many forest neighbors are there
         var neighbors = neighbor(field, rowIndex, cellIndex);
-      
-        // forest.maxPopulationSize = (neighbors.forests * rateOfPopulationIncrease) + forest.maxPopulationSize;
-        // forest.bees[0].population = (neighbors.forests * rateOfPopulationIncrease) + forest.bees[0].population
 
         // popsize(t+1) = popsize(t) * e^(intrinsic rate of increase(1-(popsize(t)/carrying capacity))+random variability in rate of increase
-        forest.bees[0].population = forest.bees[0].population * Math.pow(Math.E, intrinsicRateOfIncrease * (1-forest.bees[0].population/neighbors.capacity));
+        forest.bees[0].population = forest.bees[0].population * Math.pow(Math.E, intrinsicRateOfIncrease * (1 - forest.bees[0].population / neighbors.capacity));
 
 
         field[rowIndex][cellIndex] = forest;
@@ -85,11 +111,21 @@ var advanceBeePopulation = function(field) {
 }
 
 
+var advanceTimeStep = function() {
 
-// console.log(field);
+  // c("iterate");
+  //advance crop production
+  advanceCropProduction(field);
 
-advanceBeePopulation(field);
+  //advance bee production
+  advanceBeePopulation(field);
+  // c(field);
+  c(cropYield);
+}
 
-console.log("**********");
-console.log("revised *******");
-c(field);
+
+
+advanceTimeStep();
+advanceTimeStep();
+advanceTimeStep();
+advanceTimeStep();
