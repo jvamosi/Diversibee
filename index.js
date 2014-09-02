8df951286c67e55fc49596daf8fdab7c3d39d9f8
ginxwar <kevin.wu@gmail.com>
2014-09-02 14:58:34 -0700

    modularized neighbor

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var _ = require('underscore');
 var clone = require('clone');
+var neighbor = require('./neighbor').neighbor;
+
+console.log(neighbor);
 
 // forest block
 var F = {
@@ -29,45 +32,7 @@ var field = [
   [C, C, C]
 ];
 
-// DEFINITIONS
-// a single forest isolated block would have negative rate of change
 
-// need function to update rate of change and population
-
-
-// function to determine count of neighboring forests to a cell
-// inputs:
-//   row: row of the cell
-//   column: column number of the cell
-// return:
-//   count of neighboring forests around current cell
-var neighbor = function(row, col) {
-
-  var count = 0, // count all the forests found
-      // begin counting one row up, unless already in topmost row
-      i = row>0 ? row-1 : row,
-      // begin counting one column left, unless already in leftmost column
-      min_j = col>0 ? col-1 : col,
-      // finish counting one row down, unless already in bottommost row
-      max_i = (row>=field.length-1) ? row : row+1,
-      // finish counting one column right, unless already in rightmost column
-      max_j = (col>=field[0].length-1) ? col : col+1;
-
-  for(; i <= max_i; i++){
-    for(var j=min_j; j <= max_j; j++){
-
-      // Don't add the block you're on!
-      if (i==row && j==col) continue;
-
-      // Increase count when you find a field
-      if(field[i][j] == F) count++;
-
-    }
-  }
-
-  // Return the number of neighboring forests found
-  return count;
-}
 
 console.log("initial field *****");
 console.log(field);
@@ -98,6 +63,9 @@ var qq = function(field) {
       // console.log(field[rowIndex][cellIndex]);
       // console.log(rowIndex, cellIndex);
 
+
+      // console.log()
+
       if (cell.type === "forest") {
 
         // determine how many neighbors are there
@@ -109,8 +77,7 @@ var qq = function(field) {
 
 
         console.log(rowIndex, cellIndex);
-
-        var neighbors = neighbor(rowIndex, cellIndex);
+        var neighbors = neighbor(field, rowIndex, cellIndex);
         console.log(neighbors);
 
 
@@ -133,54 +100,7 @@ var qq = function(field) {
 
 }
 
-// determine neighboring locations
-var qqq = function(row, col) {
 
-  // // assuming field
-  // var minRow = 0;
-  // var maxRow = field[row].length;
-  // var minCol = 0;
-  // var maxCol = 
-
-  return 2;
-
-}
-
-
-
-// function to determine count of neighboring forests to a cell
-// inputs:
-//   row: row of the cell
-//   column: column number of the cell
-// return:
-//   count of neighboring forests around current cell
-var neighbor = function(row, col) {
-
-  var count = 0, // count all the forests found
-    // begin counting one row up, unless already in topmost row
-    i = row > 0 ? row - 1 : row,
-    // begin counting one column left, unless already in leftmost column
-    min_j = col > 0 ? col - 1 : col,
-    // finish counting one row down, unless already in bottommost row
-    max_i = (row >= field.length - 1) ? row : row + 1,
-    // finish counting one column right, unless already in rightmost column
-    max_j = (col >= field[0].length - 1) ? col : col + 1;
-
-  for (; i <= max_i; i++) {
-    for (var j = min_j; j <= max_j; j++) {
-
-      // Don't add the block you're on!
-      if (i == row && j == col) continue;
-
-      // Increase count when you find a field
-      if (field[i][j] == F) count++;
-
-    }
-  }
-
-  // Return the number of neighboring forests found
-  return count;
-}
 
 
 
---
+++ neighbor.js
@@ -0,0 +1,41 @@
+// DEFINITIONS
+// a single forest isolated block would have negative rate of change
+
+// need function to update rate of change and population
+
+
+// function to determine count of neighboring forests to a cell
+// inputs:
+//   row: row of the cell
+//   column: column number of the cell
+// return:
+//   count of neighboring forests around current cell
+var neighbor = function(field, row, col) {
+
+  var count = 0, // count all the forests found
+    // begin counting one row up, unless already in topmost row
+    i = row > 0 ? row - 1 : row,
+    // begin counting one column left, unless already in leftmost column
+    min_j = col > 0 ? col - 1 : col,
+    // finish counting one row down, unless already in bottommost row
+    max_i = (row >= field.length - 1) ? row : row + 1,
+    // finish counting one column right, unless already in rightmost column
+    max_j = (col >= field[0].length - 1) ? col : col + 1;
+
+  for (; i <= max_i; i++) {
+    for (var j = min_j; j <= max_j; j++) {
+
+      // Don't add the block you're on!
+      if (i == row && j == col) continue;
+
+      // Increase count when you find a field
+      if (field[i][j] == F) count++;
+
+    }
+  }
+
+  // Return the number of neighboring forests found
+  return count;
+}
+
+module.exports.neighbor = neighbor;
