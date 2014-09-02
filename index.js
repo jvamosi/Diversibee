cd8e8a26fe06d16e7c7df0c9bbdbba8007e5cb4f
Abigail Cabunoc <abigail.cabunoc@gmail.com>
2014-09-02 14:25:08 -0700

    Comment comment comment

--- index.js
+++ index.js
@@ -37,21 +37,39 @@ var field = [
 // a single forest isolated block would have negative rate of change
 
 // need function to update rate of change and population
-// need function to determine neighbor
-var neighbor = function(x, y) {
-  var count = 0,
-      i = x>0 ? x-1 : x,
-      min_j = y>0 ? y-1 : y,
-      max_i = (x>=field.length) ? x : x+1,
-      max_j = (y>=field[0].length) ? y : y+1;
+
+
+// function to determine count of neighboring forests to a cell
+// inputs:
+//   row: row of the cell
+//   column: column number of the cell
+// return:
+//   count of neighboring forests around current cell
+var neighbor = function(row, col) {
+
+  var count = 0, // count all the forests found
+      // begin counting one row up, unless already in topmost row
+      i = row>0 ? row-1 : row,
+      // begin counting one column left, unless already in leftmost column
+      min_j = col>0 ? col-1 : col,
+      // finish counting one row down, unless already in bottommost row
+      max_i = (row>=field.length) ? row : row+1,
+      // finish counting one column right, unless already in rightmost column
+      max_j = (col>=field[0].length) ? col : col+1;
 
   for(; i <= max_i; i++){
     for(var j=min_j; j <= max_j; j++){
-      if (i==x && j==y) continue;
+
+      // Don't add the block you're on!
+      if (i==row && j==col) continue;
+
+      // Increase count when you find a field
       if(field[i][j] == F) count++;
+
     }
   }
 
+  // Return the number of neighboring forests found
   return count;
 }
 
