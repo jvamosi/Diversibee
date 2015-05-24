88f6afdfe63006406d6c0ffd17dafdc45604b4fe
Kim Bratzel <kim.bratzel@gmail.com>
2015-05-24 14:16:08 +1000

    Minor code cleanup.

--- stateManagement.js
+++ stateManagement.js
@@ -30,21 +30,20 @@ function init() {
   store.blueberryBuildPrice = 100
 
   //set up initial cell animationsL
-  for(i=0; i<store.width*store.height; i++)
+  for(i=0; i<store.width*store.height; i++) {
     setAnimation(i);
+  }
   //paint the initial state of the board
   repaintBoard()
 
   //set up turn advancement
-  document.getElementById('nextTurn').onclick = advanceTurn
+  document.getElementById("nextTurn").onclick = advanceTurn
   //trigger a turn to finish configuring the board
   advanceTurn(); 
 
 }
 
-
-
-function setUpBoardState(cellsWide, cellsHigh){
+function setUpBoardState(cellsWide, cellsHigh) {
   // initializes board state
 
   var cells = [],
@@ -60,61 +59,60 @@ function setUpBoardState(cellsWide, cellsHigh){
   for(i=0; i<cellsWide*cellsHigh; i++){
     
   distanceFromSeedCell = distancefromSeed(i, cellsWide, seededCells);
-  //Does this cell initially become a forest based on the cell's seed proximity
+  //Does this cell initially become a forest based on the cell"s seed proximity
   isForest = shouldGrowForest(distanceFromSeedCell);
 
     if(isForest){
       cells[i] = {
-        type: 'forest',
+        type: "forest",
         beePop: [Math.floor(Math.random()*1000), Math.floor(Math.random()*1000), Math.floor(Math.random()*1000), Math.floor(Math.random()*1000)],
         beeGrowth: [0,0,0,0]
       }
     } else {
       cells[i] = {
-        type: 'grass',
+        type: "grass",
         beePop: [0,0,0,0],
         beeGrowth: [0,0,0,0]
-      }      
+      }
     }
-
   }
-
-  return cells
+  return cells;
 }
 
 
-function setAnimation(i){
+function setAnimation(i) {
   //set up the animation for cell i
 
   store.mapCell[i] = new createjs.Sprite(store.spriteSheet, store.state[i].type);
   store.mapCell[i].x = 20*(i%store.width);
   store.mapCell[i].y = 20*Math.floor(i/store.width);
-  store.mapCell[i].addEventListener('click', clickCell.bind(this,i) )
-  store.mapCell[i].addEventListener('mouseover', showStats.bind(this,i) )
+  store.mapCell[i].addEventListener("click", clickCell.bind(this, i));
+  store.mapCell[i].addEventListener("mouseover", showStats.bind(this, i));
 
   store.stage.addChild(store.mapCell[i]);
-  store.mapCell[i].play(store.state[i].type);  
+  store.mapCell[i].play(store.state[i].type);
 }
 
 function repaintBoard() {
   //re-paints the board state
 
-  if(store.animationLoop)
-    clearInterval(store.animationLoop)
+  if(store.animationLoop) {
+    clearInterval(store.animationLoop);
+  }
 
   store.stage.update();
   store.animationLoop = setInterval(function(){store.stage.update();}, 300);
 }
 
-function clickCell(i){
+function clickCell(i) {
   //handle when a player clicks on the ith cell
 
   //change the clicked cell to a blueberry patch
-  if(store.cash >=100 && store.state[i].type!='blueberries'){
+  if(store.cash >=100 && store.state[i].type !== "blueberries") {
     store.state[i] = {
-      type: 'blueberries',
+      type: "blueberries",
       beePop: [0,0,0,0],
-      beeGrowth: [0,0,0,0]    
+      beeGrowth: [0,0,0,0]
     }
     updateCash(-1*store.blueberryBuildPrice);
     setAnimation(i)
@@ -122,25 +120,25 @@ function clickCell(i){
   }
 }
 
-function showStats(i){
+function showStats(i) {
   //display the current state of cell i in a text box.
 
-  var text = 'Current State of cell ' + i +':<br>';
+  var text = "Current State of cell " + i +":<br>";
 
-  text += 'Type: ' + store.state[i].type + '<br>';
+  text += "Type: " + store.state[i].type + "<br>";
 
-  text += 'Bee Populations: ' + store.state[i].beePop + '<br>';
+  text += "Bee Populations: " + store.state[i].beePop + "<br>";
 
-  text += 'Bee Growth Rates: ' + store.state[i].beeGrowth
+  text += "Bee Growth Rates: " + store.state[i].beeGrowth;
 
-  document.getElementById('cellStats').innerHTML = text;
+  document.getElementById("cellStats").innerHTML = text;
 }
 
-function updateCash(income){
-  //update the farmer's bank account
+function updateCash(income) {
+  //update the farmer"s bank account
 
-  store.cash += income
-  document.getElementById('bank').innerHTML = '$'+store.cash
+  store.cash += income;
+  document.getElementById("bank").innerHTML = "$"+store.cash;
 }
 
 function advanceTurn(){
@@ -149,40 +147,40 @@ function advanceTurn(){
   var i;
 
   //update bee populations in forests & gather profits from blueberries
-  for(i=0; i<store.width*store.height; i++){
-    if(store.state[i].type == 'forest'){
+  for(i=0; i<store.width*store.height; i++) {
+    if(store.state[i].type === "forest") {
       updateBeePop(i);
-      updateBeeGrowth(i, adjacentCells(i));      
-    } else if (store.state[i].type == 'blueberries'){
+      updateBeeGrowth(i, adjacentCells(i));
+    } else if (store.state[i].type === "blueberries") {
       updateProfits(i, adjacentCells(i));
     }
   }
 }
 
-function updateBeePop(i){
+function updateBeePop(i) {
   //update the bee population in cell i based on its current growth rates
 
   var j; 
 
-  for(j=0; j<store.state[i].beePop.length; j++){
-    store.state[i].beePop[j] += store.state[i].beeGrowth[j]
-    store.state[i].beePop[j] = Math.max(0, store.state[i].beePop[j])
+  for(j=0; j<store.state[i].beePop.length; j++) {
+    store.state[i].beePop[j] += store.state[i].beeGrowth[j];
+    store.state[i].beePop[j] = Math.max(0, store.state[i].beePop[j]);
   }
 
 }
 
-function updateBeeGrowth(i, neighbours){
-  //update the growth rate for the bee populations in cell i based on effects from the cell's nearest neighbours
+function updateBeeGrowth(i, neighbours) {
+  //update the growth rate for the bee populations in cell i based on effects from the cell"s nearest neighbours
 
   baseBeeSpawning(i);
-  forestEdgeEffect(i, neighbours)
+  forestEdgeEffect(i, neighbours);
 
 }
 
-function updateProfits(i, neighbours){
+function updateProfits(i, neighbours) {
   //collect profits from cell i, modified by its nearest neghbours
 
-  updateCash(basicProfits(neighbours))
+  updateCash(basicProfits(neighbours));
 }
 
 function distancefromSeed(cell, width, seededCells) {
@@ -204,7 +202,6 @@ function distanceBetweenCells(cellA, cellB, boardWidth) {
   var xDiff = (cellA % boardWidth) - (cellB % boardWidth);
   var yDiff = (Math.floor(cellA / boardWidth)) - (Math.floor(cellB / boardWidth));
   return Math.abs(xDiff) + Math.abs(yDiff);
-
 }
 
 function shouldGrowForest(distanceFromSeed) {
--- util.js
+++ util.js
@@ -7,25 +7,25 @@ function adjacentCells(i){
   j, neighbourIndex;
 
   //three above:
-  for(j=-1; j<2; j++){
+  for(j=-1; j<2; j++) {
     neighbourIndex = i-store.width + j
-    if(neighbourIndex >= 0 && Math.floor(neighbourIndex/store.width) == Math.floor((i-store.width)/store.width) ){
+    if(neighbourIndex >= 0 && Math.floor(neighbourIndex/store.width) === Math.floor((i-store.width)/store.width)) {
       neighbours.push(store.state[neighbourIndex]);
     }
   }
 
   //two beside
-  if(i-1 > Math.floor(i/store.width)*store.width){
+  if(i-1 > Math.floor(i/store.width)*store.width) {
     neighbours.push(store.state[i-1]);
   }
-  if(i+1 < Math.floor(i/store.width)*store.width + store.width){
+  if(i+1 < Math.floor(i/store.width)*store.width + store.width) {
     neighbours.push(store.state[i+1]);
   }
 
   //three below
   for(j=-1; j<2; j++){
     neighbourIndex = i+store.width + j
-    if(neighbourIndex < store.width*store.height && Math.floor(neighbourIndex/store.width) == Math.floor((i+store.width)/store.width) ){
+    if(neighbourIndex < store.width*store.height && Math.floor(neighbourIndex/store.width) === Math.floor((i+store.width)/store.width)) {
       neighbours.push(store.state[neighbourIndex]);
     }
   }
