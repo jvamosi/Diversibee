293ecfce99565718571b9e207c83b2da07b4e8e3
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-06-04 15:53:05 +0100

    comment added - #71

--- tests/util.js
+++ tests/util.js
@@ -1,11 +1,5 @@
 /* global tests */
 tests.addSuite([
-    function() {
-        //Test if the method Utils.adjacentCells exists
-
-        tests.isA(Utils.adjacentCells, 'function');
-    },
-    function() {
         //Test if the method Utils.inArray exists
 
         tests.isA(Utils.inArray, 'function');
