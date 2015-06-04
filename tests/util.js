cbf8adf873902909cdded9282bb404e0058f2ab8
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-06-04 15:08:05 +0100

    tests added for inArray - #71

--- tests/util.js
+++ tests/util.js
@@ -10,4 +10,19 @@ tests.addSuite([
 
         tests.isA(Utils.inArray, 'function');
     },
+    function() {
+        //Test inArray
+
+        //Test if an element is in an empty array
+        tests.equals(Utils.inArray(1, []), false);
+
+        //Test if an element is not in an array
+        tests.equals(Utils.inArray(1, [2, 3]), false);
+
+        //Test if an element is in an array
+        tests.equals(Utils.inArray(1, [1, 2]), true);
+
+        //Test if an element is in an array but with a different type
+        tests.equals(Utils.inArray(1, ['1', 2]), false);
+    }
 ]);
