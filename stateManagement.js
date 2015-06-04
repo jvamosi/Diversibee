c2a5c9aa772a28e1e975cb02d3668b8aafbdf238
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-06-04 11:21:08 +0100

    tabs to spaces - #30

--- stateManagement.js
+++ stateManagement.js
@@ -1,253 +1,253 @@
 (function () {
-	//functions managing & updating the game state data, consisting of the `store` object and
-	//visualization in the browser.
+    //functions managing & updating the game state data, consisting of the `store` object and
+    //visualization in the browser.
 
-	var Diversibee = {};
+    var Diversibee = {};
 
-	/**
-	 * Private methods
-	 */
-	function setUpBoardState(cellsWide, cellsHigh) {
-		// initializes board state
+    /**
+     * Private methods
+     */
+    function setUpBoardState(cellsWide, cellsHigh) {
+        // initializes board state
 
-		var cells = [],
-			distanceFromSeedCell,
-			isForest,
-			i,
-			//The likelihood a cell contains a "seed"; probability bound 0 <= seedRate <= 1
-			seedRate = 0.02,
-			//Cells containing an initial "seed". Seeds are used to determine the inital forest growth
-			seededCells = generateUniqueRandomCells(cellsHigh, cellsWide, (cellsHigh*cellsWide*seedRate));
-
-
-		for(i=0; i<cellsWide*cellsHigh; i++){
-			distanceFromSeedCell = distancefromSeed(i, cellsWide, seededCells);
-			//Does this cell initially become a forest based on the cell"s seed proximity
-			isForest = shouldGrowForest(distanceFromSeedCell);
-
-			if(isForest){
-				cells[i] = {
-					type: "forest",
-					beePop: [Math.floor(Math.random()*1000), Math.floor(Math.random()*1000), Math.floor(Math.random()*1000), Math.floor(Math.random()*1000)],
-					beeGrowth: [0,0,0,0]
-				}
-			} else {
-				cells[i] = {
-					type: "grass",
-					beePop: [0,0,0,0],
-					beeGrowth: [0,0,0,0]
-				}
-			}
-		}
-
-		return cells;
-	}
+        var cells = [],
+            distanceFromSeedCell,
+            isForest,
+            i,
+            //The likelihood a cell contains a "seed"; probability bound 0 <= seedRate <= 1
+            seedRate = 0.02,
+            //Cells containing an initial "seed". Seeds are used to determine the inital forest growth
+            seededCells = generateUniqueRandomCells(cellsHigh, cellsWide, (cellsHigh*cellsWide*seedRate));
 
 
-	function setAnimation(i) {
-		//set up the animation for cell i
+        for(i=0; i<cellsWide*cellsHigh; i++){
+            distanceFromSeedCell = distancefromSeed(i, cellsWide, seededCells);
+            //Does this cell initially become a forest based on the cell"s seed proximity
+            isForest = shouldGrowForest(distanceFromSeedCell);
 
-		store.mapCell[i] = new createjs.Sprite(store.spriteSheet, store.state[i].type);
-		store.mapCell[i].x = 20*(i%store.width);
-		store.mapCell[i].y = 20*Math.floor(i/store.width);
-		store.mapCell[i].addEventListener("click", clickCell.bind(this, i));
-		store.mapCell[i].addEventListener("mouseover", showStats.bind(this, i));
+            if(isForest){
+                cells[i] = {
+                    type: "forest",
+                    beePop: [Math.floor(Math.random()*1000), Math.floor(Math.random()*1000), Math.floor(Math.random()*1000), Math.floor(Math.random()*1000)],
+                    beeGrowth: [0,0,0,0]
+                }
+            } else {
+                cells[i] = {
+                    type: "grass",
+                    beePop: [0,0,0,0],
+                    beeGrowth: [0,0,0,0]
+                }
+            }
+        }
 
-		store.stage.addChild(store.mapCell[i]);
-		store.mapCell[i].play(store.state[i].type);
-	}
+        return cells;
+    }
 
-	function repaintBoard() {
-		//re-paints the board state
 
-		if(store.animationLoop) {
-			clearInterval(store.animationLoop);
-		}
+    function setAnimation(i) {
+        //set up the animation for cell i
 
-		store.stage.update();
-		store.animationLoop = setInterval(function(){store.stage.update();}, 300);
-	}
+        store.mapCell[i] = new createjs.Sprite(store.spriteSheet, store.state[i].type);
+        store.mapCell[i].x = 20*(i%store.width);
+        store.mapCell[i].y = 20*Math.floor(i/store.width);
+        store.mapCell[i].addEventListener("click", clickCell.bind(this, i));
+        store.mapCell[i].addEventListener("mouseover", showStats.bind(this, i));
 
-	function clickCell(i) {
-		//handle when a player clicks on the ith cell
+        store.stage.addChild(store.mapCell[i]);
+        store.mapCell[i].play(store.state[i].type);
+    }
 
-		//change the clicked cell to a blueberry patch
-		if(store.cash >=100 && store.state[i].type !== "blueberries") {
-			store.state[i] = {
-				type: "blueberries",
-				beePop: [0,0,0,0],
-				beeGrowth: [0,0,0,0]
-			}
-			updateCash(-1*store.blueberryBuildPrice);
-			setAnimation(i)
-			repaintBoard();
-		}
-	}
+    function repaintBoard() {
+        //re-paints the board state
 
-	function showStats(i) {
-		//display the current state of cell i in a text box.
+        if(store.animationLoop) {
+            clearInterval(store.animationLoop);
+        }
 
-		var text = "Current State of cell " + i +":<br>";
+        store.stage.update();
+        store.animationLoop = setInterval(function(){store.stage.update();}, 300);
+    }
 
-		text += "Type: " + store.state[i].type + "<br>";
+    function clickCell(i) {
+        //handle when a player clicks on the ith cell
 
-		text += "Bee Populations: " + store.state[i].beePop + "<br>";
+        //change the clicked cell to a blueberry patch
+        if(store.cash >=100 && store.state[i].type !== "blueberries") {
+            store.state[i] = {
+                type: "blueberries",
+                beePop: [0,0,0,0],
+                beeGrowth: [0,0,0,0]
+            }
+            updateCash(-1*store.blueberryBuildPrice);
+            setAnimation(i)
+            repaintBoard();
+        }
+    }
 
-		text += "Bee Growth Rates: " + store.state[i].beeGrowth;
+    function showStats(i) {
+        //display the current state of cell i in a text box.
 
-		document.getElementById("cellStats").innerHTML = text;
-	}
+        var text = "Current State of cell " + i +":<br>";
 
-	function updateCash(income) {
-		//update the farmer"s bank account
+        text += "Type: " + store.state[i].type + "<br>";
 
-		store.cash += income;
-		document.getElementById("bank").innerHTML = "$"+store.cash;
-	}
+        text += "Bee Populations: " + store.state[i].beePop + "<br>";
 
-	function advanceTurn(){
-		//trigger to advance to next turn
+        text += "Bee Growth Rates: " + store.state[i].beeGrowth;
 
-		var i;
+        document.getElementById("cellStats").innerHTML = text;
+    }
 
-		//update bee populations in forests & gather profits from blueberries
-		for(i=0; i<store.width*store.height; i++) {
-			if(store.state[i].type === "forest") {
-				updateBeePop(i);
-				updateBeeGrowth(i, adjacentCells(i));
-			} else if (store.state[i].type === "blueberries") {
-				updateProfits(i, adjacentCells(i));
-			}
-		}
-	}
+    function updateCash(income) {
+        //update the farmer"s bank account
 
-	function updateBeePop(i) {
-		//update the bee population in cell i based on its current growth rates
+        store.cash += income;
+        document.getElementById("bank").innerHTML = "$"+store.cash;
+    }
 
-		var j;
+    function advanceTurn(){
+        //trigger to advance to next turn
 
-		for(j=0; j<store.state[i].beePop.length; j++) {
-			store.state[i].beePop[j] += store.state[i].beeGrowth[j];
-			store.state[i].beePop[j] = Math.max(0, store.state[i].beePop[j]);
-		}
-	}
+        var i;
 
-	function updateBeeGrowth(i, neighbours) {
-		//update the growth rate for the bee populations in cell i based on effects from the cell"s nearest neighbours
+        //update bee populations in forests & gather profits from blueberries
+        for(i=0; i<store.width*store.height; i++) {
+            if(store.state[i].type === "forest") {
+                updateBeePop(i);
+                updateBeeGrowth(i, adjacentCells(i));
+            } else if (store.state[i].type === "blueberries") {
+                updateProfits(i, adjacentCells(i));
+            }
+        }
+    }
 
-		baseBeeSpawning(i);
-		forestEdgeEffect(i, neighbours);
-	}
+    function updateBeePop(i) {
+        //update the bee population in cell i based on its current growth rates
 
-	function updateProfits(i, neighbours) {
-		//collect profits from cell i, modified by its nearest neghbours
+        var j;
 
-		updateCash(basicProfits(neighbours));
-	}
+        for(j=0; j<store.state[i].beePop.length; j++) {
+            store.state[i].beePop[j] += store.state[i].beeGrowth[j];
+            store.state[i].beePop[j] = Math.max(0, store.state[i].beePop[j]);
+        }
+    }
 
-	function distancefromSeed(cell, width, seededCells) {
-		//Returns the distance from a cell to the closest seed (seeds are given as an array of cell locations)
-		var distance = Number.MAX_SAFE_INTEGER;
-		var i;
-		var temp_dist;
+    function updateBeeGrowth(i, neighbours) {
+        //update the growth rate for the bee populations in cell i based on effects from the cell"s nearest neighbours
 
-		for(i=0; i<seededCells.length; i++){
-			temp_dist = distanceBetweenCells(seededCells[i], cell, width);
-			distance = Math.min(distance, temp_dist);
-		}
-		return distance;
-	}
+        baseBeeSpawning(i);
+        forestEdgeEffect(i, neighbours);
+    }
 
-	function distanceBetweenCells(cellA, cellB, boardWidth) {
-		//returns the distance between cell A and cell B
+    function updateProfits(i, neighbours) {
+        //collect profits from cell i, modified by its nearest neghbours
 
-		var xDiff = (cellA % boardWidth) - (cellB % boardWidth);
-		var yDiff = (Math.floor(cellA / boardWidth)) - (Math.floor(cellB / boardWidth));
-		return Math.abs(xDiff) + Math.abs(yDiff);
-	}
+        updateCash(basicProfits(neighbours));
+    }
 
-	function shouldGrowForest(distanceFromSeed) {
-		//A simple forest growth algorithm which clusters around seed locations
+    function distancefromSeed(cell, width, seededCells) {
+        //Returns the distance from a cell to the closest seed (seeds are given as an array of cell locations)
+        var distance = Number.MAX_SAFE_INTEGER;
+        var i;
+        var temp_dist;
 
-		return distanceFromSeed + distanceFromSeed * Math.random() < 5;
-	}
+        for(i=0; i<seededCells.length; i++){
+            temp_dist = distanceBetweenCells(seededCells[i], cell, width);
+            distance = Math.min(distance, temp_dist);
+        }
+        return distance;
+    }
 
-	function generateUniqueRandomCells(height, width, numberOfCells) {
-		//Returns an array of unique random cells
+    function distanceBetweenCells(cellA, cellB, boardWidth) {
+        //returns the distance between cell A and cell B
 
-		var uniqueRandomCells = [],
-			i;
-		//we can safely choose the first unique random cell number
-		var cellNumber = Math.floor(Math.random() * (height * width));
+        var xDiff = (cellA % boardWidth) - (cellB % boardWidth);
+        var yDiff = (Math.floor(cellA / boardWidth)) - (Math.floor(cellB / boardWidth));
+        return Math.abs(xDiff) + Math.abs(yDiff);
+    }
 
-		for(i=0; i<numberOfCells; i++) {
-			while ( inArray(cellNumber, uniqueRandomCells) ) {
-				cellNumber = Math.floor(Math.random() * (height * width));
-			}
-			uniqueRandomCells.push(cellNumber);
-		}
-		return uniqueRandomCells;
-	}
+    function shouldGrowForest(distanceFromSeed) {
+        //A simple forest growth algorithm which clusters around seed locations
 
-	/**
-	 * Public methods
-	 */
-	Diversibee.init = function () {
-		// Initialize the play area on load
+        return distanceFromSeed + distanceFromSeed * Math.random() < 5;
+    }
 
-		var i;
+    function generateUniqueRandomCells(height, width, numberOfCells) {
+        //Returns an array of unique random cells
 
-		//declare global store with default values
-		store = {}
-		store.width = 20
-		store.height = 20
-		store.state = setUpBoardState(store.width, store.height);
-		store.animationData = {
-			images: ["img/spriteSheet.png"],
-			frames: {width:20, height:20},
-			animations: {
-				grass: [0,3],
-				forest: [4,7],
-				blueberries: [8,11]
-			}
-		};
-		store.stage = new createjs.Stage("board");
-		store.stage.enableMouseOver(10);
-		store.mapCell = []
-		store.spriteSheet = new createjs.SpriteSheet(store.animationData);
-		store.w = store.stage.canvas.width;
-		store.h = store.stage.canvas.height;
-		store.cash = 1000
-		store.blueberryBuildPrice = 100
+        var uniqueRandomCells = [],
+            i;
+        //we can safely choose the first unique random cell number
+        var cellNumber = Math.floor(Math.random() * (height * width));
 
-		//set up initial cell animationsL
-		for(i=0; i<store.width*store.height; i++) {
-			setAnimation(i);
-		}
-		//paint the initial state of the board
-		repaintBoard()
+        for(i=0; i<numberOfCells; i++) {
+            while ( inArray(cellNumber, uniqueRandomCells) ) {
+                cellNumber = Math.floor(Math.random() * (height * width));
+            }
+            uniqueRandomCells.push(cellNumber);
+        }
+        return uniqueRandomCells;
+    }
 
-		//set up turn advancement
-		document.getElementById("nextTurn").onclick = advanceTurn
-		//trigger a turn to finish configuring the board
-		Diversibee.advanceTurn();
-	};
+    /**
+     * Public methods
+     */
+    Diversibee.init = function () {
+        // Initialize the play area on load
 
-	Diversibee.advanceTurn = function () {
-		//trigger to advance to next turn
+        var i;
 
-		var i;
+        //declare global store with default values
+        store = {}
+        store.width = 20
+        store.height = 20
+        store.state = setUpBoardState(store.width, store.height);
+        store.animationData = {
+            images: ["img/spriteSheet.png"],
+            frames: {width:20, height:20},
+            animations: {
+                grass: [0,3],
+                forest: [4,7],
+                blueberries: [8,11]
+            }
+        };
+        store.stage = new createjs.Stage("board");
+        store.stage.enableMouseOver(10);
+        store.mapCell = []
+        store.spriteSheet = new createjs.SpriteSheet(store.animationData);
+        store.w = store.stage.canvas.width;
+        store.h = store.stage.canvas.height;
+        store.cash = 1000
+        store.blueberryBuildPrice = 100
 
-		//update bee populations in forests & gather profits from blueberries
-		for(i=0; i<store.width*store.height; i++) {
-			if(store.state[i].type === "forest") {
-				updateBeePop(i);
-				updateBeeGrowth(i, adjacentCells(i));
-			} else if (store.state[i].type === "blueberries") {
-				supdateProfits(i, adjacentCells(i));
-			}
-		}
-	};
+        //set up initial cell animationsL
+        for(i=0; i<store.width*store.height; i++) {
+            setAnimation(i);
+        }
+        //paint the initial state of the board
+        repaintBoard()
 
-	window.Diversibee = Diversibee;
+        //set up turn advancement
+        document.getElementById("nextTurn").onclick = advanceTurn
+        //trigger a turn to finish configuring the board
+        Diversibee.advanceTurn();
+    };
+
+    Diversibee.advanceTurn = function () {
+        //trigger to advance to next turn
+
+        var i;
+
+        //update bee populations in forests & gather profits from blueberries
+        for(i=0; i<store.width*store.height; i++) {
+            if(store.state[i].type === "forest") {
+                updateBeePop(i);
+                updateBeeGrowth(i, adjacentCells(i));
+            } else if (store.state[i].type === "blueberries") {
+                supdateProfits(i, adjacentCells(i));
+            }
+        }
+    };
+
+    window.Diversibee = Diversibee;
 })();
