b15382e731c11ac4221b87dad0fe76744dc57957
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-06-04 15:06:19 +0100

    no return needed - #71

--- tests/util.js
+++ tests/util.js
@@ -3,11 +3,11 @@ tests.addSuite([
     function() {
         //Test if the method Utils.adjacentCells exists
 
-        return tests.isA(Utils.adjacentCells, 'function');
+        tests.isA(Utils.adjacentCells, 'function');
     },
     function() {
         //Test if the method Utils.inArray exists
 
-        return tests.isA(Utils.inArray, 'function');
+        tests.isA(Utils.inArray, 'function');
     },
 ]);
