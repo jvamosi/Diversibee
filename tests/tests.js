9953d9988b8f0266ef3d17ce983b87d330970a49
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-06-05 15:52:38 +0100

    Comments for the test library - #71

--- tests/tests.js
+++ tests/tests.js
@@ -6,19 +6,30 @@ if (typeof (require) != 'undefined') {
 var Tests = (function() {
   //Micro tests service
 
+  // The tests library
   var tests = {},
+    // the collection of test suites
     suites = [],
+    // Where to display the results (at the moment can be console.log or
+    // a DOM node
     displayElement,
+    // The result of a run of tests, 0 if ok, 1 if at least a test fails
+    // Used as exit code for cli run
     result = 0,
+    // Function to call to print the output
     output;
 
   function _equalsObject(val, expected) {
-    //Naive, quick and dirty but working object comparison
+    // Naive, quick and dirty but working object comparison
 
     return JSON.stringify(val) === JSON.stringify(expected);
   }
 
   function _dumpResult(val, linkWord, expected, equals) {
+    // Method used to dump a sum up of a test's result.
+    // It will call output() which has been set when starting to run the
+    // suite.
+
     var result = 'Expected that ' + val + linkWord + expected;
     if (equals) {
       output(result, 'success');
@@ -29,6 +40,9 @@ var Tests = (function() {
   }
 
   function _outputCLI(text, type) {
+    // Prints a result on stdout, to run the scripts in command line
+    // To be used in travis for example.
+
     switch (type) {
       case 'title':
         displayElement(text);
@@ -43,6 +57,9 @@ var Tests = (function() {
   }
 
   function _outputHTML(text, type) {
+    // Prints a result in a DOM node, by appending the text in the
+    // node's innerHTML attribute
+
     switch (type) {
       case 'title':
         displayElement.innerHTML += '<h2>' + text + '</h2>';
@@ -102,6 +119,8 @@ var Tests = (function() {
   };
 
   tests.isA = function(val, type) {
+    // Returns true if val is of type type.
+
     var equals = typeof (val) === type;
 
     _dumpResult(
@@ -119,10 +138,21 @@ var Tests = (function() {
   };
 
   tests.addSuite = function(name, suite) {
+    // Add a suite to the collection to be tested. A suite has a name
+    // and an array of functions
+
     suites.push([name, suite]);
   };
 
   tests.runTests = function(d) {
+    // Main method to run the tests, will define output from d (display
+    // element). If d is a function (for example console.log), at the
+    // moment to display the results, output will be invoked.
+    // Otherwise, d is considered as a DOM node.
+    // Note that calling runTests in a browser with console.log as param
+    // fails with the following exception:
+    // Uncaught TypeError: Illegal invocation
+
     var s, t;
 
     result = 0;
