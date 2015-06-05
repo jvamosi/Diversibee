0ae02abcb4b52011f65475a492b1fa4b8369a3ba
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-06-05 17:06:17 +0100

    Unused method - #71

--- tests/tests-cli.js
+++ tests/tests-cli.js
@@ -1,7 +1,6 @@
 /* global require, process */
 var tests = require('./tests.js').Tests;
 require('./profits.js');
-require('./util.js');
 require('./world.js');
 
 process.exit(tests.runTests(console.log));
