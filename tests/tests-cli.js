5c2b905958a53bc22e6eecadf664864ae8683b33
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-06-05 16:48:45 +0100

    tests added for the Word.Coord class - #71

--- tests/tests-cli.js
+++ tests/tests-cli.js
@@ -2,5 +2,6 @@
 var tests = require('./tests.js').Tests;
 require('./profits.js');
 require('./util.js');
+require('./world.js');
 
 process.exit(tests.runTests(console.log));
---
+++ tests/world.js
@@ -0,0 +1,48 @@
+/* global require */
+if (typeof (require) != 'undefined') {
+  var Tests = require('./tests.js').Tests,
+    World = require('../js/world.js').World;
+}
+
+Tests.addSuite('world', [
+  function() {
+    //Test if the method World.Coord exists
+
+    Tests.isA(World.Coord, 'function');
+  },
+
+  function() {
+    //Test of the method Utils.inArray exists
+
+    /*
+     * Will calculate the distance from c1 to all the others to cover
+     * all the directions
+     * c2  c3  c4
+     *    \ | /
+     *     \|/
+     * c5---c1--c6
+     *    / |\
+     *   /  | \
+     * c7  c8  c9
+     */
+    var c1 = new World.Coord(2, 2);
+    var c2 = new World.Coord(0, 0);
+    var c3 = new World.Coord(2, 0);
+    var c4 = new World.Coord(4, 0);
+    var c5 = new World.Coord(0, 2);
+    var c6 = new World.Coord(4, 2);
+    var c7 = new World.Coord(0, 4);
+    var c8 = new World.Coord(2, 4);
+    var c9 = new World.Coord(4, 4);
+
+    Tests.equals(c1.distanceFrom(c2), 2 * Math.sqrt(2));
+    Tests.equals(c1.distanceFrom(c3), 2);
+    Tests.equals(c1.distanceFrom(c4), 2 * Math.sqrt(2));
+    Tests.equals(c1.distanceFrom(c5), 2);
+    Tests.equals(c1.distanceFrom(c6), 2);
+    Tests.equals(c1.distanceFrom(c7), 2 * Math.sqrt(2));
+    Tests.equals(c1.distanceFrom(c8), 2);
+    Tests.equals(c1.distanceFrom(c9), 2 * Math.sqrt(2));
+  }
+
+]);
