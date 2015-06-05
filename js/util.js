76a0bd1efc6959c51d92fa24531117433f647958
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-06-05 12:57:52 +0100

    Unused method - #71

--- js/util.js
+++ js/util.js
@@ -4,40 +4,6 @@ var Utils = (function() {
 
   var Utils = {};
 
-  Utils.adjacentCells = function(i) {
-    //return a list of cell objects corresponding to the neighbours of cell i
-
-    var neighbours = [],
-      j, neighbourIndex;
-
-    //three above:
-    for (j = -1; j < 2; j++) {
-      neighbourIndex = i - Diversibee.store.width + j;
-      if (neighbourIndex >= 0 && Math.floor(neighbourIndex / Diversibee.store.width) === Math.floor((i - Diversibee.store.width) / Diversibee.store.width)) {
-        neighbours.push(Diversibee.store.state[neighbourIndex]);
-      }
-    }
-
-    //two beside
-    if (i - 1 > Math.floor(i / Diversibee.store.width) * Diversibee.store.width) {
-      neighbours.push(Diversibee.store.state[i - 1]);
-    }
-
-    if (i + 1 < Math.floor(i / Diversibee.store.width) * Diversibee.store.width + Diversibee.store.width) {
-      neighbours.push(Diversibee.store.state[i + 1]);
-    }
-
-    //three below
-    for (j = -1; j < 2; j++) {
-      neighbourIndex = i + Diversibee.store.width + j;
-      if (neighbourIndex < Diversibee.store.width * Diversibee.store.height && Math.floor(neighbourIndex / Diversibee.store.width) === Math.floor((i + Diversibee.store.width) / Diversibee.store.width)) {
-        neighbours.push(Diversibee.store.state[neighbourIndex]);
-      }
-    }
-
-    return neighbours;
-  };
-
   Utils.inArray = function(needle, haystack) {
     //Determine if item is in array
     //Borrowed from: http://stackoverflow.com/questions/784012/javascript-equivalent-of-phps-in-array
