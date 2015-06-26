0f2eeff71e66cb8100f2dad71761970063f82a7e
Cory Bloor <cgbloor@ucalgary.ca>
2015-06-26 13:47:45 -0600

    Add MPL license header
    These headers are standard and expected for code licenced under the
    MPL, because the license has implications on a per-file basis.
    The headers should have been added along with #90.

--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 /* global exports, require */
 if (typeof (require) != 'undefined') {
   var color = require('colors/safe');
